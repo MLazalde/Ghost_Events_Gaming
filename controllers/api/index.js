0c19b4b8fdd119750b0a2e8c93e6f4c42870003f
JayJetta1 <j_jenkins1@u.pacific.edu>
2024-07-18 11:27:37 -0700

    modified gitignore to ignore pseudo.txt

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const productRoutes = require('./product');
+const productRoutes = require("./product");
 //http://localhost:3001/api/product
-router.use('/product', productRoutes);
+router.use("/product", productRoutes);
 
 module.exports = router;
